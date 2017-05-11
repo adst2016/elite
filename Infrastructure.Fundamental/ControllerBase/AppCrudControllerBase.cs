79fcae968779f223996dbf3dd04b5f4df2b3f00b
adst2016 <adam.stachura@hotmail.com>
2017-05-11 18:20:37 +0200

    Zmiany w kontrolerze.

--- Infrastructure.Fundamental/ControllerBase/AppCrudControllerBase.cs
+++ Infrastructure.Fundamental/ControllerBase/AppCrudControllerBase.cs
@@ -22,9 +22,97 @@ namespace Infrastructure.Fundamental.ControllerBase
             this.service = service;
         }
 
-        public ActionResult Index()
+        public virtual ActionResult Index()
         {
             return View(service.GetAll());
+        }
+
+        public virtual ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var infoDto = service.GetById(id.Value);
+            if (infoDto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoDto);
+        }
+
+        public virtual ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Create(DtoCreateT createDto)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Create(createDto);
+                return RedirectToAction("Index");
+            }
+
+            return View(createDto);
+        }
+
+        public virtual ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var infoDto = service.GetById(id.Value);
+
+            if (infoDto == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(infoDto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult Edit(DtoInfoT infoDto)
+        {
+            if (ModelState.IsValid)
+            {
+                service.Update(infoDto);
+                return RedirectToAction("Index");
+            }
+            return View(infoDto);
+        }
+
+        public virtual ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var infoDto = service.GetById(id.Value);
+            if (infoDto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(infoDto);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public virtual ActionResult DeleteConfirmed(Guid id)
+        {
+            service.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
         }
     }
 }
